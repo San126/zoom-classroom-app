cf6e1fa98ced55cb50c0ecdbfde5f82b9ccefeb3
San126 <sandrarnair@gmail.com>
2023-12-30 14:08:07 +0530

    Added demo login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import ClassRoom from './ClassRoom';
+import LoginForm from './Login';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,10 +10,18 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const App = () => {
   return (
+    <>
     <div>
       <nav class="navbar navbar-light bg-light"><h4>Schedule Classroom App</h4></nav>
-      <ClassRoom />
     </div>
+     <Router>
+     <Routes>
+       {/* Default route for the login page */}
+       <Route path="/" element={<LoginForm />} />
+       <Route path="/schedule" element={<ClassRoom />} />
+     </Routes>
+   </Router>
+   </>
   );
 };
 
---
+++ client/src/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//demo login page not active
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate('/schedule');
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h3 className="text-center">Login</h3>
+            </div>
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="d-grid">
+                  <button type="submit" className="btn btn-primary">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
